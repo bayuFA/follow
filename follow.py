7debdb80b07c66f32e770656d0e91ee9f193e446
bayuFA <62793771+bayuFA@users.noreply.github.com>
2020-04-17 17:19:01 +0700

    Add files via upload

---
+++ follow.py
@@ -0,0 +1,10 @@
+<html>
+<head>
+  <title>Follow Bro</title>
+</head>
+<body>
+  <div>
+    klik tombol <span style"color: red;">&follow; <a class="link" href="https://www.instagram.com/bayufahrianas/" target="_blank" rel="nofollow">FOLLOW</a>
+  </div>
+</body>
+</html>
